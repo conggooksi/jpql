86c7708b003e8b2e7c89dccd9a1caae5661bd1c5
kuki <kimgeonkuk@gmail.com>
2022-09-08 09:45:19 +0900

    pushed by vscode

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,48 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            List<String> result = em.createQuery(query, String.class).getResultList();
+
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
